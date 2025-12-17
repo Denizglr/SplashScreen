d068de2b350f81f3186ce89ea96eacbc5839190b
Denizglr <gulerdeniz71@gmail.com>
2025-12-17 09:55:14 +0300

    Splashscreen Eklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SplashScreen2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.lblBankName = new System.Windows.Forms.Label();
+            this.lblSlogan = new System.Windows.Forms.Label();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblSurum2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlProgress = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.BackColor = System.Drawing.Color.DimGray;
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(229, 26);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(191, 164);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 0;
+            this.pbLogo.TabStop = false;
+            this.pbLogo.Click += new System.EventHandler(this.pbLogo_Click);
+            // 
+            // lblBankName
+            // 
+            this.lblBankName.AutoSize = true;
+            this.lblBankName.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBankName.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblBankName.Location = new System.Drawing.Point(205, 205);
+            this.lblBankName.Name = "lblBankName";
+            this.lblBankName.Size = new System.Drawing.Size(236, 54);
+            this.lblBankName.TabIndex = 1;
+            this.lblBankName.Text = "DNG BANK";
+            // 
+            // lblSlogan
+            // 
+            this.lblSlogan.AutoSize = true;
+            this.lblSlogan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSlogan.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblSlogan.Location = new System.Drawing.Point(161, 259);
+            this.lblSlogan.Name = "lblSlogan";
+            this.lblSlogan.Size = new System.Drawing.Size(322, 25);
+            this.lblSlogan.TabIndex = 2;
+            this.lblSlogan.Text = "YOUR SECURITY, OUR PRIORITY";
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.ForeColor = System.Drawing.Color.DimGray;
+            this.lblLoading.Location = new System.Drawing.Point(299, 314);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(65, 16);
+            this.lblLoading.TabIndex = 4;
+            this.lblLoading.Text = "Loading...";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblVersion.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblVersion.Location = new System.Drawing.Point(13, 384);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(29, 15);
+            this.lblVersion.TabIndex = 5;
+            this.lblVersion.Text = "v1.0";
+            // 
+            // lblSurum2
+            // 
+            this.lblSurum2.AutoSize = true;
+            this.lblSurum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSurum2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblSurum2.Location = new System.Drawing.Point(511, 384);
+            this.lblSurum2.Name = "lblSurum2";
+            this.lblSurum2.Size = new System.Drawing.Size(128, 15);
+            this.lblSurum2.TabIndex = 6;
+            this.lblSurum2.Text = "© 2025 ZENITH BANK";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlProgress
+            // 
+            this.pnlProgress.BackColor = System.Drawing.Color.Goldenrod;
+            this.pnlProgress.Location = new System.Drawing.Point(130, 333);
+            this.pnlProgress.Name = "pnlProgress";
+            this.pnlProgress.Size = new System.Drawing.Size(405, 17);
+            this.pnlProgress.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(650, 400);
+            this.Controls.Add(this.pnlProgress);
+            this.Controls.Add(this.lblSurum2);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.lblSlogan);
+            this.Controls.Add(this.lblBankName);
+            this.Controls.Add(this.pbLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label lblBankName;
+        private System.Windows.Forms.Label lblSlogan;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblSurum2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel pnlProgress;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen2
+{
+    public partial class Form1 : Form
+    {
+        // Yükleme Çubuğu Kontrol Değişkenleri
+        private const int MaxProgressWidth = 500; // Çubuğun ulaşacağı maksimum piksel genişliği
+        private const int MaxLoadCount = 100;     // Yüklemenin tamamlanacağı adım sayısı
+        private int currentProgress = 0;          // Mevcut yükleme adımı
+        public Form1()
+        {
+            InitializeComponent();
+            // 1. Logo Ataması (Projeye eklediğiniz görsel adı)
+            // this.pbLogo.Image = global::ProjeAdiniz.Properties.Resources.ZenithLogo;
+
+            // 2. Yükleme Çubuğu Başlangıç Ayarları
+            pnlProgress.Width = 0; // Başlangıçta görünmez (genişlik 0)
+            pnlProgress.Height = 10; // Çubuk kalınlığı (tasarımınıza göre ayarlayın)
+
+            // Yükleme çubuğunu formun yatay olarak ortasına ve dikeyde 300. piksele konumlandırır.
+            pnlProgress.Location = new Point((this.Width - MaxProgressWidth) / 2, 300);
+
+            // 3. Timer Ayarları
+            timer1.Interval = 50; // 50 milisaniyede bir Tick olayı tetiklenir (Hız)
+            timer1.Start();       // Zamanlayıcıyı hemen başlat.
+        }
+
+        private void pbLogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentProgress++;
+
+            // Yükleme çubuğunun genişliğini her adımda orantılı olarak artırır.
+            int newWidth = (int)((double)currentProgress / MaxLoadCount * MaxProgressWidth);
+
+            if (newWidth <= MaxProgressWidth)
+            {
+                pnlProgress.Width = newWidth;
+            }
+
+            // Yükleme bitti mi kontrolü (100. adımdan sonra)
+            if (currentProgress > MaxLoadCount)
+            {
+                timer1.Stop(); // Zamanlayıcıyı durdurur.
+
+                // **ANA FORMU AÇMA BÖLÜMÜ**
+                // AnaForm mainForm = new AnaForm(); // Ana formunuzun sınıf adını yazın
+                // mainForm.Show();
+
+                this.Close(); // Splash screen'i kapatır.
+            }
+            }
+    }
+}
